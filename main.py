fc24e30e39299781ac627dcfbe27628ef146317c
Jaco Steyn <jacosteyn1990@gmail.com>
2020-07-17 15:09:16 +0200

    Update main.py
    Add Battleship class and render battleships method

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+class Battleship(object):
 
+    @staticmethod
+    def build(head, length, direction):
+        body = []
+        for i in range(length):
+            if direction == "N":
+                el = (head[0], head[1] - i)
+            elif direction == "S":
+                el = (head[0], head[1] + i)
+            elif direction == "E":
+                el = (head[0] + i, head[1])
+            elif direction == "W":
+                el = (head[0] - i, head[1])
 
+            body.append(el)
+        return Battleship(body)
+
+    def __init__(self, body):
+        self.body = body
+
+# Draw the board
 def renderBoard(boardWidth, boardHeight, shots):
+    # How the top and bottom border looks
     border = "+" + "-" * boardWidth + "+"
+
     print(border)
 
+    # Parse the shots list to a set
     shots_set = set(shots)
+
     for y in range(boardHeight):
         row = []
         for x in range(boardWidth):
@@ -17,7 +41,48 @@ def renderBoard(boardWidth, boardHeight, shots):
 
     print(border)
 
+def renderBattleships(boardWidth, boardHeight, battleships):
+    # How the top and bottom border looks
+    border = "+" + "-" * boardWidth + "+"
+
+    print(border)
+
+    # Construct empty board
+    board = []
+    for _ in range(boardWidth):
+        # List comprehension
+        board.append([None for _ in range(boardHeight)])
+
+    # Add the battleships to the board
+    for b in battleships:
+        for x, y in b.body:
+            board[x][y] = "0"
+
+    for y in range(boardHeight):
+        row = []
+        for x in range(boardWidth):
+            row.append(board[x][y] or " ")
+        print("|" + "".join(row) + "|")
+
+    print(border)
+
+# Main
 if __name__ == "__main__":
+    battleships = [
+        Battleship.build((1,1), 2, "N"),
+        Battleship.build((5,8), 4, "N"),
+        Battleship.build((2,3), 3, "E")
+    ]
+
+    for b in battleships:
+        print(b.body)
+
+    renderBattleships(10, 10, battleships)
+
+    exit(0)
+
+
+    # Empty list that will hold the shots fired
     shots = []
 
     while True:
